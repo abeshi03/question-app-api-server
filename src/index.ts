a3990c60ec2a02ed36f203e2bd29d21dfa106b7d
abeshi03 <k_913411@yahoo.co.jp>
2022-05-29 17:45:26 +0900

    express, typescriptの設定完了

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/* --- フレームワーク、ライブラリー --------------------------------------------------------------------------------------- */
+import express, { Application } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app: Application = express();
+
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello world!")
+});
+
+app.get("*", (req, res) => {
+  return res.status(400).send({ error: "Invalid Url" });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./index";
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
