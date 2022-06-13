ca3bd4e61f7b3a7d86535a3ecac0713ceab28526
abeshi03 <k_913411@yahoo.co.jp>
2022-06-13 23:46:37 +0900

    fix: seed

--- prisma/seeds/models/testOptions.ts
+++ prisma/seeds/models/testOptions.ts
@@ -6,36 +6,54 @@ export const testOptionsData: Prisma.TestOptionsCreateInput[] = [
     text: "赤",
     isCorrectAnswer: true,
     createdAt: moment().format(),
-    TestQuestion: { connect: { id: 3 } }
+    TestQuestion: { connect: { id: 3 } },
   },
   {
     text: "黒",
     isCorrectAnswer: false,
     createdAt: moment().format(),
-    TestQuestion: { connect: { id: 3 } }
+    TestQuestion: { connect: { id: 3 } },
   },
   {
     text: "白",
     isCorrectAnswer: false,
     createdAt: moment().format(),
-    TestQuestion: { connect: { id: 3 } }
+    TestQuestion: { connect: { id: 3 } },
+  },
+  {
+    text: "赤",
+    isCorrectAnswer: true,
+    createdAt: moment().format(),
+    TestQuestion: { connect: { id: 4 } },
+  },
+  {
+    text: "黒",
+    isCorrectAnswer: false,
+    createdAt: moment().format(),
+    TestQuestion: { connect: { id: 4 } },
+  },
+  {
+    text: "白",
+    isCorrectAnswer: false,
+    createdAt: moment().format(),
+    TestQuestion: { connect: { id: 4 } },
   },
   {
     text: "飲食店",
     isCorrectAnswer: true,
     createdAt: moment().format(),
-    TestQuestion: { connect: { id: 4 } }
+    TestQuestion: { connect: { id: 5 } },
   },
   {
     text: "美容室",
     isCorrectAnswer: true,
     createdAt: moment().format(),
-    TestQuestion: { connect: { id: 4 } }
+    TestQuestion: { connect: { id: 5 } },
   },
   {
     text: "警察官",
     isCorrectAnswer: false,
     createdAt: moment().format(),
-    TestQuestion: { connect: { id: 4 } }
+    TestQuestion: { connect: { id: 5 } },
   },
-]
+];
