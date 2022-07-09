7cd2c21a3811c0edc2429381581fa84211bb5af8
abeshi03 <k_913411@yahoo.co.jp>
2022-07-09 09:57:57 +0900

    fix: seed

--- prisma/seeds/models/categories.ts
+++ prisma/seeds/models/categories.ts
@@ -10,4 +10,80 @@ export const categoriesData: Prisma.CategoriesCreateInput[] = [
     name: "カテゴリー2",
     createdAt: moment().format(),
   },
+  {
+    name: "カテゴリー3",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー4",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー5",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー6",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー7",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー8",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー9",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー10",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー11",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー12",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー13",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー14",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー15",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー16",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー17",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー18",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー19",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー20",
+    createdAt: moment().format(),
+  },
+  {
+    name: "カテゴリー21",
+    createdAt: moment().format(),
+  },
 ];
